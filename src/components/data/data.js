31bd7fe7ce7117be56cc384aaeb1402a5c5508d3
Miraz Ali <mirazali1998@gmail.com>
2021-07-06 18:53:29 +0530

    creating components folder contain all components

---
+++ src/components/data/data.js
@@ -0,0 +1,30 @@
+import javed from "./images/javed.jpg";
+import mobin from "./images/mobin.jpg";
+import raj from "./images/raj.jpg";
+import bibhuti from "./images/bibhuti.jpg";
+export default [
+  {
+    id: 1,
+    name: "Javed",
+    age: 24,
+    image: javed,
+  },
+  {
+    id: 2,
+    name: "Mobin",
+    age: 23,
+    image: mobin,
+  },
+  {
+    id: 3,
+    name: "Raj",
+    age: 24,
+    image: raj,
+  },
+  {
+    id: 4,
+    name: "Bibhuti",
+    age: 22,
+    image: bibhuti,
+  },
+];
